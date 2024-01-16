331946db62fcbe50f8fce6175c9d1d4b216bc0e7
jodyford-msft <jodyford-msft>
2024-01-16 16:09:26 +0000

    added a sample

---
+++ Untitled Notebook 2024-01-16 11_08_44.py
@@ -0,0 +1,12 @@
+# Databricks notebook source
+# MAGIC %matplotlib inline
+# MAGIC import matplotlib.pyplot as plt
+# MAGIC
+# MAGIC x = ['A', 'B', 'C', 'D']
+# MAGIC y = [10, 20, 30, 40]
+# MAGIC
+# MAGIC plt.bar(x, y)
+# MAGIC plt.xlabel('Categories')
+# MAGIC plt.ylabel('Values')
+# MAGIC plt.title('Bar Graph')
+# MAGIC plt.show()
